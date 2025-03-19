54763dfa0182650467fbf192c9a548b02a751e09
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2025-03-19 12:13:22 -0300

    proyecto base

---
+++ src/main/java/com/minera/minera/MineraApplication.java
@@ -0,0 +1,13 @@
+package com.minera.minera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineraApplication.class, args);
+	}
+
+}
